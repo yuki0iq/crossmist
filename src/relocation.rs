d1e063d556276488303cc64e0db90cb83b9d6a10
Yuki Sireneva <yuki.utk8g@gmail.com>
2025-07-19 13:38:46 +0300

    Peel off one indirection layer from relocation static

--- src/relocation.rs
+++ src/relocation.rs
@@ -2,9 +2,9 @@ use crate::{Deserializer, NonTrivialObject, Serializer};
 use std::io::Result;
 
 // This needs to be a singleton to prevent different codegen units from using different copies of
-// the function. See also: https://github.com/alecmocatta/relative/pull/2
-
-static BASE_ADDRESS: fn(()) = std::mem::drop::<()>;
+// the function.
+// See also: https://github.com/iex-rs/lithium/blob/9e7a1b551231/src/backend/seh.rs#L131
+static BASE_ADDRESS: () = ();
 
 #[derive(Debug)]
 #[repr(transparent)]
@@ -20,11 +20,13 @@ impl<T> Copy for RelocatablePtr<T> {}
 
 unsafe impl<T> NonTrivialObject for RelocatablePtr<T> {
     fn serialize_self_non_trivial<'a>(&'a self, s: &mut Serializer<'a>) {
-        s.serialize_temporary((self.0 as usize).wrapping_sub(BASE_ADDRESS as usize));
+        s.serialize_temporary((self.0 as usize).wrapping_sub((&raw const BASE_ADDRESS).addr()));
     }
     unsafe fn deserialize_self_non_trivial(d: &mut Deserializer) -> Result<Self> {
         Ok(Self(
-            (BASE_ADDRESS as usize).wrapping_add(d.deserialize()?) as *const T
+            (&raw const BASE_ADDRESS)
+                .addr()
+                .wrapping_add(d.deserialize()?) as *const T,
         ))
     }
 }
